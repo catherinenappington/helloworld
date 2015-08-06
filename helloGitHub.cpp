711e4626b5d1164c272b805a29c742c1ab7fb453
catherinenappington <catherinenappington@openmailbox.org>
2015-08-06 03:12:13 -0700

    Create helloGitHub.cpp
    I just finished a C++ course, so I figured I'd opt for some C++ to start with, though I'll likely use C next time.

---
+++ helloGitHub.cpp
@@ -0,0 +1,20 @@
+/*
+Made by Catherine Nappington
+Last edit: Augst 6th 2015
+For those that are reading this, what kind of information
+do you like to include in your initial comments
+and how do you format it?
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+  cout << "Hello, GitHub ! !! \n";
+  cout << "I'm new here, so please be gentle...\n";
+  
+  return 0;
+}
